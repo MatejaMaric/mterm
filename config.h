0411a837a8f6410d2c4ad213d9e881757e025e8b
Mateja <mail@matejamaric.com>
2020-12-09 23:06:08 +0100

    Change colors.

--- config.h
+++ config.h
@@ -3,8 +3,7 @@ static const int scroll_on_output = 0;
 static const int scroll_on_keystroke = 1;
 static const int mouse_autohide = 0;
 
-
-static const float transparency = 0.8;
+static const double transparency = 0.9;
 
 static const int colors[] = {
   0x3b4252,
@@ -30,6 +29,7 @@ static const int cursor_foreground = 0x2e3440;
 
 static const int foreground = 0xd8dee9;
 static const int foreground_bold = 0xd8dee9;
-static const int background = 0x2e3440;
+/* static const int background = 0x2e3440; */
+static const int background = 0x171a20;
 
 static const int highlight = 0x4c566a;
